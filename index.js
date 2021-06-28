91651bf0ac26ab465cf6d7943b348ef577aa5862
Lex Nelson <lexinelson597@gmail.com>
2021-06-28 12:39:12 -0400

    commit tree 3d37cb56429eaa9c43929b632391043b15121aac

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return 'This one is on me!'
+  } else if (number >= 2500){
+    return 'No can do.'
+  } else if (number > 2000){
+    return 'I will gladly take your thirty bucks.'
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === 'NYC' ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+  case 'generous': 
+  return 'Thank you so much.'
+  break;
+  case 'not as generous':
+  return 'Thank you.'
+  break;
+  default:
+    return 'Bye.'
+    break;
+  
+  }
 }
